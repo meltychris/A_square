2b9ab713dd59e6b59285985a0a6802c2355a06ae
stsk <stsk1v3@gmail.com>
2014-03-12 00:30:23 +0800

    newUI

---
+++ ASquare/src/com/example/asquare/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.asquare;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+        button1 = (ImageButton)findViewById(R.id.imageButton1);
+        button1.setOnClickListener(new ImageButton.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	Intent intent = new Intent();
+                intent.setClass(MainActivity.this, GPA_Calculator.class);
+                startActivity(intent);
+               
+        }});
+        
+        button2 = (ImageButton)findViewById(R.id.imageButton2);
+        button2.setOnClickListener(new ImageButton.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+               
+        }});
+        
+        button3 = (ImageButton)findViewById(R.id.imageButton3);
+        button3.setOnClickListener(new ImageButton.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+               
+        }});
+        
+        button4 = (ImageButton)findViewById(R.id.imageButton4);
+        button4.setOnClickListener(new ImageButton.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+               
+        }});
+        
+        button5 = (ImageButton)findViewById(R.id.imageButton5);
+        button5.setOnClickListener(new ImageButton.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+               
+        }});
+        
+        button6 = (ImageButton)findViewById(R.id.imageButton6);
+        button6.setOnClickListener(new ImageButton.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+               
+        }});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	private ImageButton button1;
+	private ImageButton button2;
+	private ImageButton button3;
+	private ImageButton button4;
+	private ImageButton button5;
+	private ImageButton button6;
+
+}
