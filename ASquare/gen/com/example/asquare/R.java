2f6f788477640f74716b0edd9b92a40cc2d2754d
skszeto <skszeto@CSL4WK55.win2k.cs.ust.hk>
2014-03-12 11:22:03 +0800

    UI design optimization

--- ASquare/gen/com/example/asquare/R.java
+++ ASquare/gen/com/example/asquare/R.java
@@ -24,17 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
-        public static final int imageButton1=0x7f080001;
-        public static final int imageButton2=0x7f080002;
-        public static final int imageButton3=0x7f080004;
-        public static final int imageButton4=0x7f080005;
-        public static final int imageButton5=0x7f080007;
-        public static final int imageButton6=0x7f080008;
+        public static final int action_settings=0x7f080010;
+        public static final int imageButton1=0x7f080002;
+        public static final int imageButton2=0x7f080004;
+        public static final int imageButton3=0x7f080007;
+        public static final int imageButton4=0x7f080009;
+        public static final int imageButton5=0x7f08000c;
+        public static final int imageButton6=0x7f08000e;
         public static final int linearLayout1=0x7f080000;
-        public static final int linearLayout2=0x7f080003;
-        public static final int linearLayout3=0x7f080006;
-        public static final int textView1=0x7f080009;
+        public static final int linearLayout11=0x7f080001;
+        public static final int linearLayout12=0x7f080003;
+        public static final int linearLayout2=0x7f080005;
+        public static final int linearLayout21=0x7f080006;
+        public static final int linearLayout22=0x7f080008;
+        public static final int linearLayout3=0x7f08000a;
+        public static final int linearLayout31=0x7f08000b;
+        public static final int linearLayout32=0x7f08000d;
+        public static final int textView1=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
