80b1493e5ace5d0e2362d813cd5514f50c27d71d
skszeto <skszeto@CSL4WK55.win2k.cs.ust.hk>
2014-03-14 23:17:28 +0800

    rte

--- ExistingSQLiteDatabase/src/com/existingsqlitedatabase/MainActivity.java
+++ ExistingSQLiteDatabase/src/com/existingsqlitedatabase/MainActivity.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.database.Cursor;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
