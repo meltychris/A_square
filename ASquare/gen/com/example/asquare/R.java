98b6da9c5ed9fddd8551be452ff85e441b2d1cbe
stsk <stsk1v3@gmail.com>
2014-03-15 18:37:06 +0800

    change view

--- ASquare/gen/com/example/asquare/R.java
+++ ASquare/gen/com/example/asquare/R.java
@@ -24,8 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001b;
+        public static final int action_settings=0x7f080016;
         public static final int button1=0x7f080011;
+        public static final int button2=0x7f080012;
+        public static final int button3=0x7f080013;
+        public static final int button4=0x7f080014;
         public static final int editText1=0x7f080010;
         public static final int imageButton1=0x7f080002;
         public static final int imageButton2=0x7f080004;
@@ -42,16 +45,8 @@ public final class R {
         public static final int linearLayout3=0x7f08000a;
         public static final int linearLayout31=0x7f08000b;
         public static final int linearLayout32=0x7f08000d;
-        public static final int linearLayout4=0x7f080012;
-        public static final int linearLayout5=0x7f080015;
-        public static final int linearLayout6=0x7f080018;
-        public static final int listView1=0x7f080014;
-        public static final int listView2=0x7f080017;
-        public static final int listView3=0x7f08001a;
+        public static final int listView1=0x7f080015;
         public static final int textView1=0x7f08000f;
-        public static final int year1=0x7f080013;
-        public static final int year2=0x7f080016;
-        public static final int year3=0x7f080019;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- ASquare/src/com/example/study_path/Study_Path.java
+++ ASquare/src/com/example/study_path/Study_Path.java
@@ -22,9 +22,11 @@ public class Study_Path extends Activity {
         setContentView(R.layout.path_main);       
 	    // TODO Auto-generated method stub
 	    button1 = (Button)findViewById(R.id.button1);
+	    button2 = (Button)findViewById(R.id.button2);
+	    button3 = (Button)findViewById(R.id.button3);
+	    button4 = (Button)findViewById(R.id.button4);
+	    
 	    listView1 = (ListView) findViewById(R.id.listView1);
-        listView2 = (ListView) findViewById(R.id.listView2);
-       	listView3 = (ListView) findViewById(R.id.listView3);
        	edittext1 = (EditText) findViewById(R.id.editText1);
 
         button1.setOnClickListener(new Button.OnClickListener(){ 
@@ -32,9 +34,65 @@ public class Study_Path extends Activity {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
             	listView1.setAdapter(get(edittext1.getText().toString(), "1"));
-            	listView2.setAdapter(get(edittext1.getText().toString(), "2"));
-        		listView3.setAdapter(get(edittext1.getText().toString(), "3"));
-
+            	
+            	/*String A = data.get(edittext1.getText().toString(), 1);
+            	textView2.setText(A);  
+            	String B = data.get(edittext1.getText().toString(), 2);
+            	textView3.setText(B);  
+            	String C = data.get(edittext1.getText().toString(), 3);
+            	textView4.setText(C);  
+            	
+            	
+            	if	(C.equals(" "))
+            			Toast.makeText(Study_Path.this, "Wrong Major", Toast.LENGTH_LONG).show();
+            */
+        		}
+        });
+        
+        button2.setOnClickListener(new Button.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	listView1.setAdapter(get(edittext1.getText().toString(), "1"));
+            	/*String A = data.get(edittext1.getText().toString(), 1);
+            	textView2.setText(A);  
+            	String B = data.get(edittext1.getText().toString(), 2);
+            	textView3.setText(B);  
+            	String C = data.get(edittext1.getText().toString(), 3);
+            	textView4.setText(C);  
+            	
+            	
+            	if	(C.equals(" "))
+            			Toast.makeText(Study_Path.this, "Wrong Major", Toast.LENGTH_LONG).show();
+            */
+        		}
+        });
+        
+        button3.setOnClickListener(new Button.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	listView1.setAdapter(get(edittext1.getText().toString(), "2"));
+            	
+            	/*String A = data.get(edittext1.getText().toString(), 1);
+            	textView2.setText(A);  
+            	String B = data.get(edittext1.getText().toString(), 2);
+            	textView3.setText(B);  
+            	String C = data.get(edittext1.getText().toString(), 3);
+            	textView4.setText(C);  
+            	
+            	
+            	if	(C.equals(" "))
+            			Toast.makeText(Study_Path.this, "Wrong Major", Toast.LENGTH_LONG).show();
+            */
+        		}
+        });
+        
+        button4.setOnClickListener(new Button.OnClickListener(){ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	listView1.setAdapter(get(edittext1.getText().toString(), "3"));
             	
             	/*String A = data.get(edittext1.getText().toString(), 1);
             	textView2.setText(A);  
@@ -93,8 +151,9 @@ public class Study_Path extends Activity {
 		return adapter;
 	}
     private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
     private EditText edittext1;
-    private ListView listView2;
-    private ListView listView3;
     private ListView listView1;
 }
