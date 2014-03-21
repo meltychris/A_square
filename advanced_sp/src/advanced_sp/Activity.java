346a3d7854d0d574de263c462463c3dd3ad21439
mrdoi <myhsieh@stu.ust.hk>
2014-03-22 03:49:43 +0800

    test2

--- advanced_sp/src/advanced_sp/Activity.java
+++ advanced_sp/src/advanced_sp/Activity.java
@@ -14,9 +14,17 @@ public class Activity {
 		{
 			UserInfor.studentinput();
 			
-			Advancedpath.suggestion();
+			if(UserInfor.Major.equals("COMP"))
+			{
+				Comp_Advancedpath.suggestion();
+				Comp_Advancedpath.printout();
+			}
+			else if (UserInfor.Major.equals("CPEG"))
+			{
+				Cpeg_Advancedpath.suggestion();
+				Cpeg_Advancedpath.printout();
+			}
 			
-			Advancedpath.printout();
 		}
 		else
 			System.out.println("Thx and bye");
@@ -87,7 +95,7 @@ class UserInfor{
 	static public String Fail;
 }
 
-class Advancedpath extends UserInfor{
+class Comp_Advancedpath extends UserInfor{
 	public static void suggestion(){	
 		
 		if ((Credneed/(3 - Year))>=42)
@@ -97,6 +105,17 @@ class Advancedpath extends UserInfor{
 		if (Pure.equals("N") == true&& Year == 1 )
 		{
 			Year2 = Year2+"MATH 2011"+"MATH 2111";
+		}
+		
+		if (Year==1)
+		{
+			if (Fail.equals(Year1)==true)
+				Year2 = Year2 + Fail;
+		}
+		else if (Year==2)
+		{
+			if (Fail.equals(Year1)||Fail.equals(Year2)==true)
+				Year3 = Year3 + Fail;
 		}
 		
 	}
@@ -112,10 +131,36 @@ class Advancedpath extends UserInfor{
 		System.out.println("Suggest Year1 study:");
 		System.out.println(Year3);
 	}
-	static String Year1 = "nothing";
-	static String Year2 = "nothing";
-	static String Year3 = "nothing";
-	static String Path;
+	static String Year1 = "Select course code from database where recommended year = year1";
+	static String Year2 = "Select course code from database where recommended year = year2";
+	static String Year3 = "Select course code from database where recommended year = year3";
+	static int Credneed = 90 - Credone;
+	static boolean Defer;
+}
+
+class Cpeg_Advancedpath extends UserInfor{
+	public static void suggestion(){
+		Scanner input3 = new Scanner(System.in);
+		System.out.println("As CPEG student, choose COMP or ELEC approach?");
+		approach= input3.nextLine();
+	}
+	
+	public static void printout(){
+		if (Defer == true)
+			System.out.println("Sorry you need to defer");
+		else System.out.println("Congraduation! you can grad on time");
+		
+		System.out.println("Suggest Year1 study:");
+		System.out.println(Year1);
+		System.out.println("Suggest Year2 study:");
+		System.out.println(Year2);
+		System.out.println("Suggest Year1 study:");
+		System.out.println(Year3);
+	}
+	static String approach;
+	static String Year1 = "Select course code from database where recommended year = year1";
+	static String Year2 = "Select course code from database where recommended year = year2";
+	static String Year3 = "Select course code from database where recommended year = year3";
 	static int Credneed = 90 - Credone;
 	static boolean Defer;
 }
