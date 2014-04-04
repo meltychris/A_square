28aa25fb176c2e1cb1c13e50c8b67828e572db16
chrislol <ie20072007@gmail.com>
2014-04-05 01:32:27 +0800

    initial commit

---
+++ advising/src/com/example/advising/advising.java
@@ -0,0 +1,10 @@
+package com.example.advising;
+
+import java.io.*;
+
+public class advising{
+	public static void main(String[] args) {
+		System.out.println("Welcome to psychological adivising function");
+		
+	}
+}
---
+++ advising/src/com/example/advising/advisor.java
@@ -0,0 +1,12 @@
+package com.example.advising;
+
+import java.io.*;
+
+public class advisor{
+	String name;
+	int telephone;
+	String email;
+	String address;
+	String type;
+}
+
