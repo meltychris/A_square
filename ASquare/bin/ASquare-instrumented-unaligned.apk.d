91de802d2e4cc666664ec4c63cb1f0012c4ecc74
stsk <skszeto@stu.ust.hk>
2014-04-13 03:40:47 +0800

    aa

---
+++ ASquare/bin/ASquare-instrumented-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\stsk\git\A_square\ASquare\bin\ASquare-instrumented-unaligned.apk : \
+C:\Users\stsk\git\A_square\ASquare\bin\ASquare.ap_ \
+C:\Users\stsk\git\A_square\ASquare\bin\classes.dex \
+C:\Users\stsk\Desktop\adt-bundle-windows-x86-20131030\sdk\tools\lib\emma_device.jar \
---
+++ ASquare_Test/bin/classes.dex.d
@@ -0,0 +1,7 @@
+C:\Users\stsk\git\A_square\ASquare_Test\bin\classes.dex : \
+C:\Users\stsk\git\A_square\ASquare_Test\bin\classes\com\example\asquare\test\BuildConfig.class \
+C:\Users\stsk\git\A_square\ASquare_Test\bin\classes\com\example\asquare\test\R$attr.class \
+C:\Users\stsk\git\A_square\ASquare_Test\bin\classes\com\example\asquare\test\R$drawable.class \
+C:\Users\stsk\git\A_square\ASquare_Test\bin\classes\com\example\asquare\test\R$string.class \
+C:\Users\stsk\git\A_square\ASquare_Test\bin\classes\com\example\asquare\test\R.class \
+C:\Users\stsk\git\A_square\ASquare_Test\bin\classes\com\example\asquare\test\testing.class \
