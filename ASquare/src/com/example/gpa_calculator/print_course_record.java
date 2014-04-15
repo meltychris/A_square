b007f2d7b758432e963acafe87a5410d3f6a3c12
stsk <skszeto@stu.ust.hk>
2014-04-15 16:11:41 +0800

    1556456564

--- ASquare/src/com/example/gpa_calculator/print_course_record.java
+++ ASquare/src/com/example/gpa_calculator/print_course_record.java
@@ -1,5 +1,20 @@
 package com.example.gpa_calculator;
 
-public class print_course_record {
+import com.example.asquare.R;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class print_course_record extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gpa_print_course_record);
+		
+		
+	}
 
 }
