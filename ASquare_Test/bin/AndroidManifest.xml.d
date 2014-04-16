45d11cbf34e8d5b9700d6659bb0ff9e4786aa75b
stsk <skszeto@stu.ust.hk>
2014-04-16 18:12:50 +0800

    33

--- ASquare_Test/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\stsk\git\A_square\ASquare_Test\bin\AndroidManifest.xml : \
-C:\Users\stsk\git\A_square\ASquare_Test\AndroidManifest.xml \
--- ASquare_Test/bin/classes/R.java.d
+++
@@ -1,7 +0,0 @@
-C:\Users\stsk\git\A_square\ASquare_Test\gen\com\example\asquare\test\R.java \
- : C:\Users\stsk\git\A_square\ASquare_Test\res\drawable-hdpi\ic_launcher.png \
-C:\Users\stsk\git\A_square\ASquare_Test\res\drawable-ldpi\ic_launcher.png \
-C:\Users\stsk\git\A_square\ASquare_Test\res\drawable-mdpi\ic_launcher.png \
-C:\Users\stsk\git\A_square\ASquare_Test\res\drawable-xhdpi\ic_launcher.png \
-C:\Users\stsk\git\A_square\ASquare_Test\res\values\strings.xml \
-C:\Users\stsk\git\A_square\ASquare_Test\bin\AndroidManifest.xml \
