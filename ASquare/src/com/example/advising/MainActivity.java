8dafb17464ba0add74ced1a05dcc9d6b05d4842e
ie2007 <ie20072007@gmail.com>
2014-05-04 23:08:30 +0800

    intent path fixing

--- ASquare/src/com/example/advising/MainActivity.java
+++ ASquare/src/com/example/advising/MainActivity.java
@@ -20,8 +20,7 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Intent intent = new Intent();
-				intent.setClass(MainActivity.this,
-						com.example.advising.ustTeam.class);
+				intent.setClass(MainActivity.this, ustTeamActivity.class);
 				startActivity(intent);
 			}
 		});
@@ -32,55 +31,40 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Intent intent = new Intent();
-				intent.setClass(MainActivity.this,
-						com.example.advising.otherAdvising.class);
+				intent.setClass(MainActivity.this, otherActivity1.class);
 				startActivity(intent);
 			}
 		});
 	}
-/*		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-*/
+	/*
+	 * if (savedInstanceState == null) {
+	 * getSupportFragmentManager().beginTransaction() .add(R.id.container, new
+	 * PlaceholderFragment()).commit(); } }
+	 * 
+	 * @Override public boolean onCreateOptionsMenu(Menu menu) {
+	 * 
+	 * // Inflate the menu; this adds items to the action bar if it is present.
+	 * getMenuInflater().inflate(R.menu.main, menu); return true; }
+	 * 
+	 * @Override public boolean onOptionsItemSelected(MenuItem item) { // Handle
+	 * action bar item clicks here. The action bar will // automatically handle
+	 * clicks on the Home/Up button, so long // as you specify a parent activity
+	 * in AndroidManifest.xml. int id = item.getItemId(); if (id ==
+	 * R.id.action_settings) { return true; } return
+	 * super.onOptionsItemSelected(item); }
+	 * 
+	 * /** A placeholder fragment containing a simple view.
+	 * 
+	 * public static class PlaceholderFragment extends Fragment {
+	 * 
+	 * public PlaceholderFragment() { }
+	 * 
+	 * @Override public View onCreateView(LayoutInflater inflater, ViewGroup
+	 * container, Bundle savedInstanceState) { View rootView =
+	 * inflater.inflate(R.layout.fragment_main, container, false); return
+	 * rootView; } }
+	 */
 	private Button button1;
 	private Button button2;
 }
