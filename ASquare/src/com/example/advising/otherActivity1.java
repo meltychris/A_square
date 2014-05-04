07f62cf7f440883963699d00a9c303ba34fe0b14
ie2007 <ie20072007@gmail.com>
2014-05-04 23:00:20 +0800

    change in activities in advising function

--- ASquare/src/com/example/advising/otherActivity1.java
+++ ASquare/src/com/example/advising/otherActivity1.java
@@ -8,12 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ustTeamActivity extends Activity {
+public class otherActivity1 extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.advising_ust);
+		setContentView(R.layout.advising_other1);
 
 		button1 = (Button) findViewById(R.id.button1);
 		button1.setOnClickListener(new Button.OnClickListener() {
@@ -21,13 +21,26 @@ public class ustTeamActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Intent myIntentDial = new Intent(Intent.ACTION_DIAL, Uri
-						.parse("tel:23586696"));
+						.parse("tel:27720047"));
 
 				startActivity(myIntentDial);
 			};
 		});
+
+		button2 = (Button) findViewById(R.id.button2);
+		button2.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(otherActivity1.this,
+						otherActivity2.class);
+				startActivity(intent);
+			}
+		});
+
 	}
 
 	private Button button1;
-
+	private Button button2;
 }
--- ASquare/src/com/example/advising/otherActivity2.java
+++ ASquare/src/com/example/advising/otherActivity2.java
@@ -8,26 +8,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ustTeamActivity extends Activity {
+public class otherActivity2 extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.advising_ust);
+		setContentView(R.layout.advising_other2);
 
-		button1 = (Button) findViewById(R.id.button1);
-		button1.setOnClickListener(new Button.OnClickListener() {
+		button3 = (Button) findViewById(R.id.button3);
+		button3.setOnClickListener(new Button.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Intent myIntentDial = new Intent(Intent.ACTION_DIAL, Uri
-						.parse("tel:23586696"));
+						.parse("tel:27976888"));
 
 				startActivity(myIntentDial);
 			};
 		});
+	
+
 	}
 
-	private Button button1;
-
+	private Button button3;
 }
