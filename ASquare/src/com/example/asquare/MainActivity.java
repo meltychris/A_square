8a6204532622718bbdd3f62635a8dc9803070681
brianho1992 <brianho1992@gmail.com>
2014-05-04 14:02:23 +0800

    correct the GPA package mainactivity file name in com.example.asquare.MainActivity.java

--- ASquare/src/com/example/asquare/MainActivity.java
+++ ASquare/src/com/example/asquare/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
             	Intent intent = new Intent();
-                intent.setClass(MainActivity.this, com.example.gpa_calculator.GPAMainActivity.class);
+                intent.setClass(MainActivity.this, com.example.gpa_calculator.MainActivity.class);
                 startActivity(intent);
                
         }});
